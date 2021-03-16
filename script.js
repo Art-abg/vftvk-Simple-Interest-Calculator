f9489eaa688bc741af5608fa5124853706f1346e
Artur Abgaryan <artur.abg1@gmail.com>
2021-03-16 20:07:56 +0500

    update 0.5

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate");
+    var y = document.getElementById("years");
+    var interest = p * y * r / 100
+
+    var dateobj = new Date();
+    var B = dateobj.getFullYear();
+    year = B+y;
+    document.getElementById("result").innerHTML = y;
 }
-        
+function showVal(newVal){
+    document.getElementById("demo").innerHTML=newVal;
+    var slider1 = document.getElementById("myRange");
+    var demo = document.getElementById("demo");
+    demo.innerHTML = slider1.value;
+}
